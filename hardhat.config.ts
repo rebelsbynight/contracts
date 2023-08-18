d0df71ad8e53e0e14e2f5ebe097ef4922b5cb3c8
Francois Martin <contact@francois-martin.com>
2023-08-18 13:31:23 +0200

    Moved API key to dedicated file + updated config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
-require('dotenv').config();
+import("dotenv/config");
 
-require("@nomiclabs/hardhat-etherscan");
+import("@nomiclabs/hardhat-etherscan");
 import("@nomiclabs/hardhat-ethers");
 import("@nomiclabs/hardhat-waffle");
 import("hardhat-gas-reporter");
@@ -13,17 +13,14 @@ import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
-};
-module.exports = {
-  solidity: "0.8.4",
   networks: {
     mainnet: {
-    url: "https://mainnet.infura.io/v3/18ea48985537440891ba50c52e7254c3",
-    allowUnlimitedContractSize: true
+      url: "https://mainnet.infura.io/v3/18ea48985537440891ba50c52e7254c3",
+      allowUnlimitedContractSize: true
     }
   },
   etherscan: {
-    apiKey: "G3ZTD3FTKF4HPUDYN6FYIC8H19YEAY1EYI"
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
 
